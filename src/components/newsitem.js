415d600a70121b363e64aad2774db8b1580db3de
nikhilraj01 <usernikhil001@gmail.com>
2020-09-24 17:34:12 +0530

    Update

--- src/components/newsitem.js
+++ src/components/newsitem.js
@@ -1,11 +1,11 @@
 import React from "react"
-export function Newsitem({item})  
+export function Newsitem({item})
 {
     if(item!==null || item!==undefined){
     return (
     <div>
         <div className="newsItem">
-        {item.title} 
+        {item.title}
         <a style={{color:"grey"}} href={item.url} > ({item.url})</a>
         <p >{item.score} points | {item.by} | {convertTime(item.time)} | {item.descendants} comments</p>
         <p>{item.type}</p>
